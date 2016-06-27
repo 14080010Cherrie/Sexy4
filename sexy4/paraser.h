ea0636a3940d26b564fc7cd445f442d98bb5280c
14080121 <494772621@qq.com>
2016-06-27 16:02:10 +0800

    Update
    Add some input function for new files structure.

--- sexy4/paraser.h
+++ sexy4/paraser.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <map>
 using namespace std;
 using namespace minijson;
 #include "Items.h"
@@ -25,3 +26,42 @@ Items Decode(char * source)
 	return obj;
 }
 
+map<string, Item> DecodeIndex(char * source)
+{
+	map<string, Item> indexes;
+	Item item;
+	buffer_context ctx(source, strlen(source));
+	minijson::parse_object(ctx, [&](const char* name, value v)
+	{
+		minijson::dispatch(name)
+		<< minijson::any >> [&]
+		{
+			string barcode = name;
+			item.setBarcode(barcode);
+			parse_object(ctx, [&](const char* k, value v)
+			{
+				dispatch(k)
+					<< "name" >> [&] { item.setName(v.as_string()); }
+					<< "unit" >> [&] {item.setUnit(v.as_string()); }
+					<< "price" >> [&] {item.setPrice(v.as_double()); }
+					<< "discount" >> [&] {item.setDiscount(v.as_double()); }
+					<< "promotion" >> [&] {item.setPromotion(v.as_bool()); }
+					<< minijson::any >> [&] { minijson::ignore(ctx); };
+			});
+			indexes.insert(pair<string,Item>(barcode,item));
+			minijson::ignore(ctx);
+		};
+	});
+	return indexes;
+}
+
+vector<string> DecodeItems(char *source)
+{
+	vector<string> barcodes;
+	buffer_context ctx(source, strlen(source));
+	parse_array(ctx, [&](value v)
+	{
+		barcodes.push_back(v.as_string());
+	});
+	return barcodes;
+}
